572354984cd0689b556fdcd394ef81aaf8e0a520
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-01-18 14:58:53 +0100

    doctrine

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,30 +15,34 @@ class MovieController extends AbstractController
     /**
      * @Route("", name="app_movie_index")
      */
-    public function index(): Response
+    public function index(MovieRepository $repository): Response
     {
+        $movies = $repository->findAll();
+
         return $this->render('movie/index.html.twig', [
             'controller_name' => 'MovieController',
+            'movies' => $movies,
         ]);
     }
 
     /**
      * @Route("/{id}", name="app_movie_details", requirements={"id": "\d+"})
      */
-    public function details(int $id): Response
+    public function details(int $id, MovieRepository $repository): Response
     {
-        $movie = [
-            'id' => $id,
-            'title' => 'Star Wars - Episode IV : A New Hope',
-            'year' => '1977',
-            'released' => new \DateTimeImmutable('25 May 1977'),
-            'country' => 'United States',
-            'genre' => [
-                'Action',
-                'Adventure',
-                'Fantasy',
-            ],
-        ];
+        $movie = $repository->find($id);
+
+        return $this->render('movie/details.html.twig', [
+            'movie' => $movie,
+        ]);
+    }
+
+    /**
+     * @Route("/title/{title}", name="app_movie_title")
+     */
+    public function showByTitle(string $title, MovieRepository $repository): Response
+    {
+        $movie = $repository->findOneBy(['title' => $title]);
 
         return $this->render('movie/details.html.twig', [
             'movie' => $movie,
---
+++ src/Entity/Comment.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Book::class, inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $book;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getBook(): ?Book
+    {
+        return $this->book;
+    }
+
+    public function setBook(?Book $book): self
+    {
+        $this->book = $book;
+
+        return $this;
+    }
+}
